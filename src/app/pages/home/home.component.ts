23c9ee8b42859b0e9f647c816112ef780f713029
Lizeth Martinez <lizethmartinez404@gmail.com>
2025-03-30 20:40:19 -0500

    feature: Diseño de pagina home
    - Se crea el diseño de la pagina de inicio.
    - Se crean componentes del footer y el nabvar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent { }
+export class HomeComponent { 
+
+  cards = [
+    { title: 'Medicina General', image: 'stethoscope.png' },
+    { title: 'Odontología', image: 'dentistry.png' },
+    { title: 'Pediatría', image: 'pediatrics.png' },
+    { title: 'Ortopedia', image: 'orthopedics.png' }
+  ];
+
+  reviews = [
+    { rating: 5, comment: 'Excelente app, muy fácil de usar y con información precisa. Me ayuda a llevar un control adecuado de mi salud. ¡La recomiendo al 100%!' },
+    { rating: 4, comment: 'Me encanta esta aplicación. Tiene un diseño intuitivo y me permite gestionar mis citas y resultados médicos de manera rápida y segura. ¡Una herramienta esencial!' },
+    { rating: 4.5, comment: 'Esta app ha sido muy útil para mi bienestar. Tiene recordatorios de medicamentos y seguimiento de mis consultas médicas. ¡Estoy muy satisfecha!' }
+  ];
+
+  getStars(rating: number): string[] {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStar;
+
+    return [
+      ...Array(fullStars).fill('bi-star-fill'),
+      ...Array(halfStar).fill('bi-star-half'),
+      ...Array(emptyStars).fill('bi-star')
+    ];
+  }
+
+}
