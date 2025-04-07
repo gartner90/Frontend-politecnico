90b8cd5224960f50954271af2e89f0a3fed196a4
Nicolás Miranda <jnicolas.miranda12@gmail.com>
2025-04-06 21:48:50 -0500

    Actualización de rutas en app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { AppointmentsComponent } from './admin/appointments/appointments.component';
+import { NewAppointmentComponent } from './views/admin/new-appointment/new-appointment.component';
+import { EditAppointmentComponent } from './views/admin/edit-appointment/edit-appointment.component';
+import { UsersComponent } from './views/admin/users/users.component'; 
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'appointments',
+    pathMatch: 'full'
+  },
+  {
+    path: 'appointments',
+    component: AppointmentsComponent
+  },
+  {
+    path: 'new-appointment',
+    component: NewAppointmentComponent
+  },
+  {
+    path: 'edit-appointment',
+    component: EditAppointmentComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  }
+];
