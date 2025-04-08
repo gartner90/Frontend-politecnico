cfa8dbe0c4b3c5b31763bda24ebe239e7a093462
Cristian Gartner <gartner90@gmail.com>
2025-04-07 22:15:21 -0500

    connect profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { ProfileComponent } from './admin/profile/profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   {
---
+++ src/app/services/profile/profile.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { SimulateHttpService } from '../simulate-http/simulate-http.service';
+import { IUser } from '../../shared/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  private readonly simulateHttpService = inject(SimulateHttpService);
+
+  async updateUser(prevEmail: string, form: IUser) {
+    const data = await firstValueFrom(this.simulateHttpService.get('/users'));
+    const index = data.findIndex((user: any) => user?.email === prevEmail);
+    if (index < 0) throw new Error('No se encontro el usuario logueado');
+    await firstValueFrom(this.simulateHttpService.delete('/users', index));
+    await firstValueFrom(
+      this.simulateHttpService.post('/users', { ...data[index], ...form })
+    );
+  }
+}
